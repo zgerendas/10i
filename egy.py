0ca6de6b5ed58eb2840d86f605bdca260c07df4e
Gerendás Zoltán <gerendas.zoltan@wmtech.hu>
2023-09-11 15:54:13 +0200

    első

---
+++ egy.py
@@ -0,0 +1,20 @@
+print("Szia, üdv itt!")
+print("Ez egy",end=" ***** ")
+print('program')
+print('*'*10)
+print('alma '*10) # ez működik
+# print('mama'/2)   hibás utasítás
+#print("mama"-"ma")
+#print("mama"-2)
+#print("mama"**3)
+#print("mama"^3)
+print('-'*10)
+print('*'*-10) # mintha print() lenne.
+print('-'*10)
+print("al\n\n\nma")
+print("al\nma") # új sor
+print('\'')
+print('\\')
+print("\"")
+print('\x33')
+print('\xa4')
